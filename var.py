8eb32ff81d11aee7e443667cf52f8ca3f4e74e06
Labani Behera <101978891+ZxLbni@users.noreply.github.com>
2025-03-17 21:13:53 +0530

    Update var.py

--- var.py
+++ var.py
@@ -2,19 +2,19 @@
 
 import os
 
-API_ID = os.getenv("API_ID", "")
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-ADMIN = int(os.getenv("ADMIN", ""))
+API_ID = os.getenv("API_ID", "29382018")
+API_HASH = os.getenv("API_HASH", "4734a726c04620c61ec0a28a1ae0d57f")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8035534743:AAEoRY7p2i_MVMh2AJ4VgffFgjPuZKoLYGM")
+ADMIN = int(os.getenv("ADMIN", "7442532306"))
 
-CHNL_LINK = os.getenv("CHNL_LINK", "https://t.me/DypixxTech")
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-100xxxxxxx"))
-DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-100xxxxxxx"))
+CHNL_LINK = os.getenv("CHNL_LINK", "https://t.me/noob_project")
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002428562251"))
+DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-1002428562251"))
 
-DB_URI = os.getenv("DB_URI", "mongodb+srv://xxxx:xxxx@xxxx")
+DB_URI = os.getenv("DB_URI", "mongodb+srv://mrnoobx:DAZCdTczVWyECi04@cluster0.sedgwxy.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.getenv("DB_NAME", "instalaoder")
 
-IS_FSUB = bool(os.environ.get("FSUB", True)) # Set "True" For Enable Force Subscribe
+IS_FSUB = bool(os.environ.get("FSUB", False)) # Set "True" For Enable Force Subscribe
 AUTH_CHANNELS = os.environ.get("AUTH_CHANNEL", "-100xxxxxxx") # Add Multiple Channels iD By Space
 AUTH_CHANNELS = [int(channel_id) for channel_id in AUTH_CHANNELS.split(",")] # DONT TOUCH
 
